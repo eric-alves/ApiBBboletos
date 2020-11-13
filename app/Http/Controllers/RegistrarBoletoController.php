48b9b97270f776cf00710b3fd4b9551f66f046eb
eric dos reis alves <eralves01@hotmail.com>
2020-11-13 18:12:54 -0300

    atualizado

--- app/Http/Controllers/RegistrarBoletoController.php
+++ app/Http/Controllers/RegistrarBoletoController.php
@@ -69,14 +69,18 @@ class RegistrarBoletoController extends Controller
 		
 		// URL para obtenção da token para testes
 		$_urlToken = 'https://oauth.hm.bb.com.br/oauth/token';
-		// URL para registro de boleto para teste
 		$_urlRegistro = 'https://cobranca.homologa.bb.com.br:7101/registrarBoleto';
 
-        $_timeout = 20;
-        
+		$_timeout = 20;
+		
+        // Dados de Teste
         //$_clientID = 'eyJpZCI6IjgwNDNiNTMtZjQ5Mi00YyIsImNvZGlnb1B1YmxpY2Fkb3IiOjEwOSwiY29kaWdvU29mdHdhcmUiOjEsInNlcXVlbmNpYWxJbnN0YWxhY2FvIjoxfQ';
         //$_secret = 'eyJpZCI6IjBjZDFlMGQtN2UyNC00MGQyLWI0YSIsImNvZGlnb1B1YmxpY2Fkb3IiOjEwOSwiY29kaWdvU29mdHdhcmUiOjEsInNlcXVlbmNpYWxJbnN0YWxhY2FvIjoxLCJzZXF1ZW5jaWFsQ3JlZGVuY2lhbCI6MX0';
-    
+	
+		// Dados de Produção
+		//$_clienteID = 'eyJpZCI6IjcwNCIsImNvZGlnb1B1YmxpY2Fkb3IiOjAsImNvZGlnb1NvZnR3YXJlIjoxMTk3NSwic2VxdWVuY2lhbEluc3RhbGFjYW8iOjF9';
+		//$_secret = 'eyJpZCI6IjczOGQ4MzctMzk5Ni00YmRmLTg1NmMtMGMiLCJjb2RpZ29QdWJsaWNhZG9yIjowLCJjb2RpZ29Tb2Z0d2FyZSI6MTE5NzUsInNlcXVlbmNpYWxJbnN0YWxhY2FvIjoxLCJzZXF1ZW5jaWFsQ3JlZGVuY2lhbCI6MSwiYW1iaWVudGUiOiJwcm9kdWNhbyIsImlhdCI6MTYwNDkyNjYwNTY1NX0';
+
 	    $curl = curl_init();
 		curl_setopt_array($curl, array(
 			CURLOPT_BINARYTRANSFER => true,
@@ -100,6 +104,8 @@ class RegistrarBoletoController extends Controller
         curl_close($curl);
         
 		$resultado = json_decode($resposta);
+
+		#return ['dados' => $resultado];
 		
 		if(isset($resultado->error)){
 			return ['error' => $resultado->error, 'error_description' => $resultado->error_description];
@@ -116,6 +122,8 @@ class RegistrarBoletoController extends Controller
 
 		// Fecha o nó da requisição, o corpo da mensagem e o envelope
 		$requisicao .= '</xsd:requisicao></SOAP-ENV:Body></SOAP-ENV:Envelope>';
+
+		//return ['req' => $requisicao];
 
 		// Preparar requisição
 		$curl = curl_init();
@@ -154,17 +162,17 @@ class RegistrarBoletoController extends Controller
 			} else
 				$resultado = false;
 		} else {
-			$this->_erro = 'Não foi possível conectar-se ao Banco do Brasil';
-			return false;
+			return ['error' => 'Não foi possível conectar-se ao Banco do Brasil'];
 		}
 
 		// Se ocorreu tudo bem, sai
-		if (is_array($resultado) && array_key_exists('codigoRetornoPrograma', $resultado) && $resultado['codigoRetornoPrograma'] == 0)
-			return $resultado;
-        
-		return ['Token' => $resultado];
-		//return ['XML_DATA' => $resposta];
-		
+		if (is_array($resultado) && array_key_exists('codigoRetornoPrograma', $resultado) && $resultado['codigoRetornoPrograma'] == 0) {
+			$resultado['codBarImg'] = self::geraCodigoBarra($resultado['codigoBarraNumerico']);
+			return ['boleto' => $resultado];
+		} else {
+			return ['error' => $resultado];
+		}
+     
 	}
 	
 	static private function _converterNosXMLEmArray($no, &$resultado) {
@@ -173,5 +181,85 @@ class RegistrarBoletoController extends Controller
 			self::_converterNosXMLEmArray($pos, $resultado[$pos->localName]);
 		else
 			$resultado = html_entity_decode(trim($no->nodeValue));
+	}
+
+	static private function geraCodigoBarra($numero){
+		$fino = 2;
+		$largo = 4;
+		$altura = 70;
+		
+		$preta = 'data:image/gif;base64,R0lGODlhCgBQAIAAAAAAAP///yH5BAUUAAEALAAAAAAKAFAAAAIahI+py+0Po5y02ouz3rz7D4biSJbmiabqihUAOw==';
+		$branca = 'data:image/gif;base64,R0lGODlhCgBQAIAAAP///////yH5BAUUAAEALAAAAAAKAFAAAAIahI+py+0Po5y02ouz3rz7D4biSJbmiabqihUAOw==';
+		
+		$barcodes[0] = '00110';
+		$barcodes[1] = '10001';
+		$barcodes[2] = '01001';
+		$barcodes[3] = '11000';
+		$barcodes[4] = '00101';
+		$barcodes[5] = '10100';
+		$barcodes[6] = '01100';
+		$barcodes[7] = '00011';
+		$barcodes[8] = '10010';
+		$barcodes[9] = '01010';
+		
+		for($f1 = 9; $f1 >= 0; $f1--){
+			for($f2 = 9; $f2 >= 0; $f2--){
+				$f = ($f1*10)+$f2;
+				$texto = '';
+				for($i = 1; $i < 6; $i++){
+					$texto .= substr($barcodes[$f1], ($i-1), 1).substr($barcodes[$f2] ,($i-1), 1);
+				}
+				$barcodes[$f] = $texto;
+			}
+		}
+		
+		$codBarImg = '';
+
+		$codBarImg .= '<img src="'.$preta.'" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		$codBarImg .= '<img src="'.$branca.'" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		$codBarImg .= '<img src="'.$preta.'" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		$codBarImg .= '<img src="'.$branca.'" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		
+		$codBarImg .= '<img ';
+		
+		$texto = $numero;
+		
+		if((strlen($texto) % 2) <> 0){
+			$texto = '0'.$texto;
+		}
+		
+		while(strlen($texto) > 0){
+			$i = round(substr($texto, 0, 2));
+			$texto = substr($texto, strlen($texto)-(strlen($texto)-2), (strlen($texto)-2));
+			
+			if(isset($barcodes[$i])){
+				$f = $barcodes[$i];
+			}
+			
+			for($i = 1; $i < 11; $i+=2){
+				if(substr($f, ($i-1), 1) == '0'){
+  					$f1 = $fino ;
+  				}else{
+  					$f1 = $largo ;
+  				}
+  				
+  				$codBarImg .= 'src="'.$preta.'" width="'.$f1.'" height="'.$altura.'" border="0">';
+  				$codBarImg .= '<img ';
+  				
+  				if(substr($f, $i, 1) == '0'){
+					$f2 = $fino ;
+				}else{
+					$f2 = $largo ;
+				}
+				
+				$codBarImg .= 'src="'.$branca.'" width="'.$f2.'" height="'.$altura.'" border="0">';
+				$codBarImg .= '<img ';
+			}
+		}
+		$codBarImg .= 'src="'.$preta.'" width="'.$largo.'" height="'.$altura.'" border="0" />';
+		$codBarImg .= '<img src="'.$branca.'" width="'.$fino.'" height="'.$altura.'" border="0" />';
+		$codBarImg .= '<img src="'.$preta.'" width="1" height="'.$altura.'" border="0" />';
+
+		return $codBarImg;
 	}
 }
